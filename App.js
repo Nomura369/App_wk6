b108aa92336f1439b2bcfaf1b18807e97319ddca
Jenny <jenny369314@gmail.com>
2024-04-06 19:11:22 +0800

    HelloState

--- App.js
+++ App.js
@@ -1,17 +1,41 @@
-import 'react-native-gesture-handler';
-import { Platform } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import Navigation from "./src/navigation/index";
-import { GluestackUIProvider } from '@gluestack-ui/themed';
-import { config } from '@gluestack-ui/config';
+import { useState } from "react";
+import {
+  GluestackUIProvider,
+  Center,
+  HStack,
+  Button,
+  ButtonText,
+  Text,
+} from "@gluestack-ui/themed";
+import { config } from "@gluestack-ui/config";
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
-    <SafeAreaProvider>
-      <GluestackUIProvider config={config}>
-        <Navigation />   
-      </GluestackUIProvider>           
-    </SafeAreaProvider>
+    <GluestackUIProvider config={config}>
+      <Center flex={1}>
+        <HStack space="3xl">
+          <Button
+            onPress={() => setCounter(counter + 1)}
+          >
+            <ButtonText size="3xl" color="white">
+              +
+            </ButtonText>
+          </Button>
+          <Button
+            onPress={() => setCounter(counter - 1)}
+          >
+            <ButtonText size="3xl" color="white">
+              -
+            </ButtonText>
+          </Button>
+        </HStack>
+        <Text size="3xl" mt="$10">
+          {counter}
+        </Text>
+      </Center>
+    </GluestackUIProvider>
   );
 };
 
